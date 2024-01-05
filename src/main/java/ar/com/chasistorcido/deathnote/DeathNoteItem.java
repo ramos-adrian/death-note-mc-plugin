00d149b11c2206b962a37ffdf1a59cb316b12875
Ramos Adrián <ramosadrian@pm.me>
2024-01-05 20:29:11 -0300

    Add custom nbt to DeathNoteItem

--- src/main/java/ar/com/chasistorcido/deathnote/DeathNoteItem.java
+++ src/main/java/ar/com/chasistorcido/deathnote/DeathNoteItem.java
@@ -2,8 +2,12 @@ package ar.com.chasistorcido.deathnote;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class DeathNoteItem {
     private static ItemStack deathNote;
@@ -17,8 +21,15 @@ public class DeathNoteItem {
             deathNote = new ItemStack(Material.WRITABLE_BOOK);
             ItemMeta im = deathNote.getItemMeta();
             im.setDisplayName(ChatColor.WHITE + "Death Note");
+            PersistentDataContainer pdc = im.getPersistentDataContainer();
+            JavaPlugin plugin = getMainPluginInstance();
+            pdc.set(new NamespacedKey(plugin, "DeathNote"), PersistentDataType.BOOLEAN, true);
             deathNote.setItemMeta(im);
         }
         return deathNote;
+    }
+
+    private static JavaPlugin getMainPluginInstance() {
+        return JavaPlugin.getPlugin(DeathNote.class);
     }
 }
