2d73660cce8796b39d42ad326b402e37529faabc
Ramos Adrián <ramosadrian@pm.me>
2024-01-06 09:16:25 -0300

    Make mobs name in red

--- src/main/java/ar/com/chasistorcido/deathnote/ShinigamiEyesScoreBoard.java
+++ src/main/java/ar/com/chasistorcido/deathnote/ShinigamiEyesScoreBoard.java
@@ -1,5 +1,6 @@
 package ar.com.chasistorcido.deathnote;
 
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
@@ -18,8 +19,9 @@ public class ShinigamiEyesScoreBoard {
         if (scoreboard == null) {
             scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
             Team team = scoreboard.registerNewTeam(TEAM_NAME);
-            Team noTeam = scoreboard.registerNewTeam(NO_TEAM_NAME);
             team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.FOR_OTHER_TEAMS);
+            team.color(NamedTextColor.DARK_RED);
+            Team noTeam = scoreboard.registerNewTeam(NO_TEAM_NAME);
             team.setAllowFriendlyFire(false);
             noTeam.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
             return scoreboard;
